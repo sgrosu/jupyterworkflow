de970a041c0d27141cce63f0d7a383ae0c82dae5
Sebastian Grosu <bastigrosu@yahoo.com>
2017-10-12 14:42:30 +0100

    BUG: fixed bug in date parsing

--- test/test_data.py
+++ test/test_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from jupyterworkflow.data import get_fremont_data
 
 
@@ -6,3 +7,4 @@ def test_get_data():
     data = get_fremont_data()
     assert(all(data.columns == ['West','East','Total']))
     assert(isinstance(data.index, pd.DatetimeIndex))
+    assert(len(np.unique(data.index.time)) == 24)
